17c8f971f6eac81310d6ced231f3de48d0bb26be
GuMoon <wjdrngur12@kaist.ac.kr>
2021-05-05 16:06:11 +0900

    upload

---
+++ update_future_price.bat
@@ -0,0 +1,7 @@
+@ECHO ON
+title Daishin Start
+
+cd C:\Users\GuMoon\Creon-Datareader
+
+call activate py36_32
+python creon_datareader_cli.py --db_file_path D:\db\daishin\future\raw\future_price(1min).db --tick_unit 1min --product_type future
