14f1f39a34fc8a86750d3bf85dc4f44ac2613c47
GuMoon <wjdrngur12@kaist.ac.kr>
2021-05-07 20:51:23 +0900

    upload

--- update_5_min_future_price.bat
+++ update_5_min_future_price.bat
@@ -5,3 +5,5 @@ cd C:\Users\GuMoon\Creon-Datareader
 
 call activate py36_32
 python creon_datareader_cli.py --db_file_path D:\db\daishin\future\raw\future_price(5min).db --tick_unit 5min --product_type future
+
+cmd.exe
--- update_future_price.bat
+++ update_future_price.bat
@@ -5,3 +5,5 @@ cd C:\Users\GuMoon\Creon-Datareader
 
 call activate py36_32
 python creon_datareader_cli.py --db_file_path D:\db\daishin\future\raw\future_price(1min).db --tick_unit 1min --product_type future
+
+cmd.exe
